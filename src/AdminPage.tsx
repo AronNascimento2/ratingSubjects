c013671d7aaa6fc0d90d479f082bd2157694e640
Aronunicargo <avinicius@unicargo.com.br>
2025-01-23 09:51:17 -0300

    feat: add new route

--- src/AdminPage.tsx
+++ src/AdminPage.tsx
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-nocheck
+
 import { useEffect, useState } from "react";
 import { collection, addDoc, getDocs } from "firebase/firestore";
 import { toast, ToastContainer } from "react-toastify";
