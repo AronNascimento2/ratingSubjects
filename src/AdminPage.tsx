f3cb38fcbb00d2273865e0bd507179513e751217
Aronunicargo <avinicius@unicargo.com.br>
2025-01-24 15:13:36 -0300

    fix: responsivity

--- src/AdminPage.tsx
+++ src/AdminPage.tsx
@@ -15,6 +15,7 @@ import { db } from "./firebaseConfig";
 import { getFaceAndColor } from "./getFaceAndColor";
 import { calculateAverage } from "./calculateAverage";
 import { useNavigate } from "react-router-dom";
+import "./admin.css"; // Adicione um arquivo de estilo para as media queries
 
 const AdminPage = () => {
   const [subjects, setSubjects] = useState([]);
@@ -55,102 +56,33 @@ const AdminPage = () => {
     localStorage.removeItem("isAuthenticated");
   };
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        marginTop: "10px",
-      }}
-    >
-      <div
-        className="card"
-        style={{
-          width: "900px",
-          maxWidth: "100%",
-          borderRadius: "8px",
-          display: "flex",
-          justifyContent: "center",
-          flexDirection: "column",
-          alignItems: "center",
-          background: "white",
-          boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
-          boxSizing: "border-box",
-          padding: "20px",
-        }}
-      >
-        <h1 style={{ margin: 0 }}>Administração </h1>
-        <p style={{ fontWeight: "bold" }}>Adicionar Disciplina</p>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            width: "200px",
-          }}
-        >
+    <div className="container">
+      <div className="card">
+        <h1>Administração</h1>
+        <p>Adicionar Disciplina</p>
+        <div className="container-add-subject">
           <input
             className="input"
             type="text"
             placeholder="Nome da disciplina"
             value={subjectName}
             onChange={(e) => setSubjectName(e.target.value)}
-            style={{
-              padding: "10px",
-              borderRadius: "4px",
-              border: "1px solid #ccc",
-            }}
           />
-          <button
-            className="button-send"
-            onClick={addSubject}
-            style={{
-              // padding: "10px",
-              borderRadius: "4px",
-              backgroundColor: "#007BFF",
-              color: "white",
-              border: "none",
-              cursor: "pointer",
-            }}
-          >
+          <button className="button-send" onClick={addSubject}>
             Adicionar
           </button>
         </div>
-        <p style={{ fontWeight: "bold" }}>Disciplinas Avaliadas</p>
-        <div
-          style={{
-            width: "100%",
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "start",
-            gap: "10px",
-          }}
-        >
+        <p>Disciplinas Avaliadas</p>
+        <div className="disciplinas-container">
           {subjects.map((subject) => (
-            <div
-              key={subject.id}
-              style={{
-                width: "100%",
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center",
-                background: "white",
-                border: "1px solid gray",
-                borderRadius: "8px",
-                marginBottom: "10px",
-                padding: "10px",
-                boxSizing: "border-box",
-              }}
-            >
+            <div key={subject.id} className="subject-card">
               <strong>{subject.name}</strong>
               <div
                 style={{
                   display: "flex",
                   alignItems: "center",
                   justifyContent: "center",
-                  background: "#f0f0f0", // Cor de fundo mais suave
-                  // padding: "8px", // Mais espaçamento para melhorar a estética
+                  background: "#f0f0f0",
                   marginBottom: "10px",
                   borderRadius: "8px",
                   width: "200px",
@@ -165,13 +97,17 @@ const AdminPage = () => {
                     <>
                       <div style={{ fontWeight: "bold", color: "#333" }}>
                         Média:{" "}
-                      </div>{" "}
+                      </div>
                       <p style={{ color, margin: 0, fontSize: "18px" }}>
                         {average}
-                      </p>{" "}
-                      <p style={{ color, margin: 0, fontSize: "14px" }}>/ 10</p>{" "}
+                      </p>
+                      <p style={{ color, margin: 0, fontSize: "14px" }}>/ 10</p>
                       <div
-                        style={{ color, display: "flex", alignItems: "center" }}
+                        style={{
+                          color,
+                          display: "flex",
+                          alignItems: "center",
+                        }}
                       >
                         {icon}
                       </div>
@@ -179,40 +115,12 @@ const AdminPage = () => {
                   );
                 })()}
               </div>
-
-              <div
-                style={{
-                  gap: "10px",
-                  display: "flex",
-                  flexDirection: "column",
-                  width: "100%",
-                  overflow: "auto",
-                  height: "320px",
-                }}
-              >
+              <div className="subject-ratings">
                 {subject.ratings?.map((rating, index) => {
                   const { icon, color } = getFaceAndColor(rating.rating);
                   return (
-                    <div
-                      key={index}
-                      style={{
-                        width: "100%",
-                        border: "1px solid gray",
-                        padding: "10px",
-                        borderRadius: "8px",
-                        background: "#f9f9f9",
-                        boxSizing: "border-box",
-                        wordWrap: "break-word",
-                      }}
-                    >
-                      <span
-                        style={{
-                          color,
-                          display: "flex",
-                          alignItems: "center",
-                          gap: "5px",
-                        }}
-                      >
+                    <div key={index} className="rating-item">
+                      <span style={{ color }}>
                         <p>Nota: {rating.rating}</p> {icon}
                       </span>
                       <span>{rating.comment}</span>
@@ -223,17 +131,10 @@ const AdminPage = () => {
             </div>
           ))}
         </div>
-
         <ToastContainer />
       </div>
-      <div
-        style={{
-          position: "absolute",
-          top: "5px",
-          right: "16px",
-        }}
-      >
-        <button onClick={() => handleLogout()} className="button-send login">
+      <div className="logout-button">
+        <button onClick={handleLogout} className="button-send login">
           Sair
         </button>
       </div>
